7462728bf2238a328c982bf415731dec33013c36
SwixGaming <82118300+SwixGaming@users.noreply.github.com>
2021-09-27 12:10:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <html>
 <head>
 
-<meta name="keywords" content="Dont believe grayden I made the website, This website will be changed if you want new website name text me: 845-238-4552">
-<title>Ultan.me - <?echo $title;?></title>
+<meta content="Dont believe grayden I made the website, This website will be changed if you want new website name text me: 845-238-4552">
   
 </body>
 </html>
